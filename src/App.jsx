ab8e3d7900d6e645d76734164d817816fe67e652
Osokoya Fiyinfoluwa David <osokoyafiyinfoluwa@gmail.com>
2025-12-22 23:08:40 +0100

    Add CSS File

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      {/* <Navbar /> */}
+      <main className="app-main">
+        <div className="player-section"></div>
+        <div className="content-section"></div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
